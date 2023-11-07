dc07368cd7e53a455530153da655147b66b0b363
chavito017 <kb3223739115@gmail.com>
2023-11-07 17:28:10 -0500

    trabajo terminado

---
+++ app/src/test/java/com/xcheko51x/frasessimpson_ejemplo/ExampleUnitTest.kt
@@ -0,0 +1,11 @@
+package com.xcheko51x.frasessimpson_ejemplo
+
+import org.junit.Test
+
+import org.junit.Assert.*
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
